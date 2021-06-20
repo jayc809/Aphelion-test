3ae0af6158705349f6a020a0655fb7302bdba4ca
Jay Chiang <jaychiang0809@berkeley.edu>
2021-06-20 21:19:28 +0800

    Initial Commit

---
+++ Aphelion1/Aphelion1App.swift
@@ -0,0 +1,17 @@
+//
+//  Aphelion1App.swift
+//  Aphelion1
+//
+//  Created by Jay on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Aphelion1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aphelion1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aphelion1
+//
+//  Created by Jay on 2021/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
